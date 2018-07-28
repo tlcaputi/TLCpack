7000f76df6d947f9cbd0c32c67145f352e2acaee
Theodore Caputi <tcaputi@gmail.com>
2018-07-28 13:05:50 -0400

    Testing batch update

--- documentPackage.R
+++ documentPackage.R
@@ -1,6 +1,6 @@
-install.packages("devtools")
-library("devtools")
-devtools::install_github("klutometis/roxygen")
+# pacman::p_load(devtools)
+# devtools::install_github("klutometis/roxygen")
+library(devtools)
 library(roxygen2)
 
 setwd("C:/Users/tcapu/Google Drive")
