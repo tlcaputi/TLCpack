4bf5f3e831a77743a2e517da7bb0da8584b25178
Theodore Caputi <tcaputi@gmail.com>
2018-09-21 12:16:06 -0400

    add start function to tests

--- R/testingFunctions.R
+++ R/testingFunctions.R
@@ -1,21 +1,23 @@
 #' Test what's not in the list
 #'
-#' This function creates a table of summary statistics. 
-#' @param data Data vector 
+#' This function creates a table of summary statistics.
+#' @param data Data vector
 #' @param string URL string with "<TEST>" where you want to insert the data
 #' @param sample number to try
 #' @param test default to FALSE
-#' @keywords 
-#' @export 
+#' @keywords
+#' @export
 #' @examples
 #'  testOutside(y$meetid, string="http://co.milesplit.com/meets/<TEST>", sample=20, test=F)
-#'  
+#'
 
-testOutside <- function(data, string="http://co.milesplit.com/meets/<TEST>", sample=20, test=F){
+testOutside <- function(data, string="http://co.milesplit.com/meets/<TEST>", sample=20, start=NULL, test=F){
   poss_ids <- 1:max(data, na.rm=T)
   missing_ids <- poss_ids[!(poss_ids %in% data)]
   set.seed(1234)
   test_set <- sample(missing_ids, 100000, replace=F)
+
+  if (!is.null(start)) assign("wefrdbhjuifbedjhi", start, envir=.GlobalEnv)
   if(!exists("wefrdbhjuifbedjhi", envir=.GlobalEnv)) {
     assign("wefrdbhjuifbedjhi", 0, envir=.GlobalEnv)
   } else{
@@ -32,21 +34,22 @@ testOutside <- function(data, string="http://co.milesplit.com/meets/<TEST>", sam
 
 #' Test what's in the list
 #'
-#' This function creates a table of summary statistics. 
+#' This function creates a table of summary statistics.
 #' @param data Dataframe
-#' @param col Name of the column to pull from 
+#' @param col Name of the column to pull from
 #' @param string URL string with "<TEST>" where you want to insert the data
 #' @param sample number to try
 #' @param test default to FALSE
-#' @keywords 
-#' @export 
+#' @keywords
+#' @export
 #' @examples
 #'  testInside(y$meetid, col="schoolid", string="http://co.milesplit.com/teams/<TEST>", sample=1, test=F)
-#'  
+#'
 
-testInside <- function(data, col="schoolid", string="http://co.milesplit.com/teams/<TEST>", sample=1, test=F){
+testInside <- function(data, col="schoolid", string="http://co.milesplit.com/teams/<TEST>", sample=1, start=NULL, test=F){
   set.seed(1234)
   test_set <- sample(data[,col], 1000, replace=F)
+  if (!is.null(start)) assign("nhjdr4nh3498n43h289", start, envir=.GlobalEnv)
   if(!exists("nhjdr4nh3498n43h289", envir=.GlobalEnv)) {
     assign("nhjdr4nh3498n43h289", 0, envir=.GlobalEnv)
   } else{
@@ -62,13 +65,13 @@ testInside <- function(data, col="schoolid", string="http://co.milesplit.com/tea
 
 #' Read Pipe Delimited Files
 #'
-#' This function creates a table of summary statistics. 
-#' @param filename filename for pipe-delimited file 
+#' This function creates a table of summary statistics.
+#' @param filename filename for pipe-delimited file
 #' @keywords read pipe
-#' @export 
+#' @export
 #' @examples
 #'  read.pipe(filename)
-#'  
+#'
 
 
 read.pipe <- function(filename, sep="|", header=T, stringsAsFactors=F, quote="", ...) {
@@ -76,13 +79,12 @@ read.pipe <- function(filename, sep="|", header=T, stringsAsFactors=F, quote="",
 
 #' As numeric
 #'
-#' This function creates a table of summary statistics. 
-#' @param x Data 
+#' This function creates a table of summary statistics.
+#' @param x Data
 #' @keywords summary
-#' @export 
+#' @export
 #' @examples
 #'  an("45678")
-#'  
+#'
 
 an <- function(x) as.numeric(as.character(x))
-
