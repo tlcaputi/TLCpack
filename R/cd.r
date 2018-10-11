87fedbb987fb9d726600800d658b47f778254efd
Theodore Caputi <tcaputi@gmail.com>
2018-10-11 11:57:38 -0400

    wrote new files to cd.r

--- R/cd.r
+++ R/cd.r
@@ -8,6 +8,29 @@
 #' cd()
 
 
+
+cd <- function(directory){
+  if(dir.exists(directory)) { # if the directory exists
+    setwd(directory) # set the working directory
+    } else { # if it doesn't do exist
+      dir.create(directory) # create the directory
+      setwd(directory) # then set the working directory
+    }
+    return(directory) ## also return the working directory
+}
+
+
+
+#' Change Directory
+#'
+#' This function allows you to set a directory, whether it exists or not.
+#' @param directory The directory you want to set.
+#' @keywords directory
+#' @export
+#' @examples
+#' cd()
+
+
 read_feather0 <- function(data, ...) as.data.frame(read_feather(data, ...))
 
 #' Change Directory
@@ -266,6 +289,7 @@ xvars <- function(data, variables) {
   names(data) <- tolower(names(data))
   return(as.data.frame(data[,intersect(names(data), variables)]))
 }
+
 
 
 
