2d159275500658ac177edc12baa74b35777f3e7c
Theodore Caputi <tcaputi@gmail.com>
2018-10-16 13:46:08 -0400

    add yrfun to cleandata function

--- R/trackProjectFunctions.r
+++ R/trackProjectFunctions.r
@@ -152,108 +152,6 @@ hypFinder <- function(x, before=T){
 }
 
 
-#' Comprehensively clean/standardize data
-#'
-#' @param
-#' @keywords
-#' @export
-#' @examples
-
-
-cleanData <- function(x){
-  print0("renaming columns")
-  names(x) <- tolower(names(x))
-  print0("to lower case")
-  x <- apply(x,2,tolower)
-  print0("to data frame")
-  x <- as.data.frame(x)
-  print0("school name procedure")
-  if ("school" %in% names(x)){
-    for (q in c("hs", "high", "school", "academy", "the", "a",
-     "highschool", "senior", "sr", "schl")){
-      pat <- paste0("\\<", q, "\\>")
-      print0(paste0("removing ", pat))
-      x$unischool<- gsub(pat, "", x$school)
-    }
-  }
-
-  print0("name procedure")
-  if ("name" %in% names(x)){
-    nmNum <- which(names(x)=="name")[1]
-    for (q in c("sr.", "sr", "senior", "jr.","jr","junior","mr.", "mrs.","ms.","miss",
-                "iii","ii","iv","v","vi","mr","mrs","ms")) {
-                  pat <- paste0("\\<", q, "\\>")
-                  print0(paste0("removing ", pat))
-                  x$name <- gsub(pat, "", x$name)
-                }
-
-    print0("first names")
-    x$firstname <- sapply(x$name, fnFinder)
-    print0("last names")
-    x$lastname <- sapply(x$name, lnFinder)
-    print0("uninames")
-    x$uniname <- paste0(word(x$firstname, 1), " ", word(x$lastname, 1))
-    print0("before hyp")
-    x$beforehyp <- sapply(x$name, function(x) hypFinder(x, before=T))
-    print0("after hyp")
-    x$afterhyp <- sapply(x$name, function(x) hypFinder(x, before=F))
-
-    nmNum <- which(names(x) %in% c("name", "firstname","lastname","uniname"))
-    print0("replacing hyps")
-    x[,-nmNum] <- apply(x[,-nmNum], 2, function(h) gsub("-", " ", h))
-
-  } else {
-    x <- apply(x, 2, function(h) gsub("-", " ", h))
-  }
-
-  print0("removing punct")
-  x <- apply(x,2,function(h) removePunctuation(h))
-  print0("stripping white space")
-  x <- apply(x,2,function(h) stripWhitespace(h))
-  print0("trimming white space")
-  x <- apply(x,2,function(h) trimws(h))
-  print0("numbers only")
-  x <- apply(x,2,function(h) suppressWarnings(ifelse(numbers_only(h), as.numeric(as.character(h)), h)))
-  print0("done")
-  return(as.data.frame(x))
-}
-
-
-
-#' CSV to Feather (if necessary)
-#'
-#' @param
-#' @keywords
-#' @export
-#' @examples
-
-c2f <- function(in_fn, out_fn=in_fn, clean=F, pipe=F){
-  # f <- read.csv(paste0(in_fn, ".csv"), header=T, stringsAsFactor=F)
-  # assign(f, read.csv(paste0(in_fn, ".csv"), header=T, stringsAsFactor=F), envir=.GlobalEnv)
-  if(pipe){
-    read <- function(...) read.pipe(...)
-  } else {
-    read <- function(...) read.csv(...)
-  }
-
-  if (clean) {
-    print("reading from csv, cleaning, and writing to feather")
-    f <- read(paste0(in_fn, ".csv"), header=T, stringsAsFactor=F)
-    f <- cleanData(f)
-    write_feather0(f, paste0(out_fn, ".feather"))
-  } else {
-    feather_name <- paste0(in_fn,".feather")
-    if(file.exists(feather_name)) {
-      print("feather file already exists, just reading from disk")
-      f <- read_feather0(feather_name)
-    } else {
-      print("file doesn't exist -- reading file from csv, writing to feather")
-      f <- read(paste0(in_fn, ".csv"), header=T, stringsAsFactor=F)
-      write_feather0(f, paste0(out_fn, ".feather"))
-    }
-  }
-  return(f)
-}
 
 
 
@@ -363,4 +261,110 @@ read.pipe <- function(filename, ...) read.delim(filename, sep="|", header=T, quo
 wn <- function(x){
   ax <- sapply(as.numeric(x), FUN = function(h) strsplit(as.character(h), "[.]")[[1]][1])
   return(as.character(ax))
+}
+
+
+#' Comprehensively clean/standardize data
+#'
+#' @param
+#' @keywords
+#' @export
+#' @examples
+
+
+cleanData <- function(x){
+  print0("renaming columns")
+  names(x) <- tolower(names(x))
+  print0("to lower case")
+  x <- apply(x,2,tolower)
+  print0("to data frame")
+  x <- as.data.frame(x)
+  print0("school name procedure")
+  if ("school" %in% names(x)){
+    for (q in c("hs", "high", "school", "academy", "the", "a",
+     "highschool", "senior", "sr", "schl")){
+      pat <- paste0("\\<", q, "\\>")
+      print0(paste0("removing ", pat))
+      x$unischool<- gsub(pat, "", x$school)
+    }
+  }
+
+  print0("name procedure")
+  if ("name" %in% names(x)){
+    nmNum <- which(names(x)=="name")[1]
+    for (q in c("sr.", "sr", "senior", "jr.","jr","junior","mr.", "mrs.","ms.","miss",
+                "iii","ii","iv","v","vi","mr","mrs","ms")) {
+                  pat <- paste0("\\<", q, "\\>")
+                  print0(paste0("removing ", pat))
+                  x$name <- gsub(pat, "", x$name)
+                }
+
+    print0("first names")
+    x$firstname <- sapply(x$name, fnFinder)
+    print0("last names")
+    x$lastname <- sapply(x$name, lnFinder)
+    print0("uninames")
+    x$uniname <- paste0(word(x$firstname, 1), " ", word(x$lastname, 1))
+    print0("before hyp")
+    x$beforehyp <- sapply(x$name, function(x) hypFinder(x, before=T))
+    print0("after hyp")
+    x$afterhyp <- sapply(x$name, function(x) hypFinder(x, before=F))
+
+    nmNum <- which(names(x) %in% c("name", "firstname","lastname","uniname"))
+    print0("replacing hyps")
+    x[,-nmNum] <- apply(x[,-nmNum], 2, function(h) gsub("-", " ", h))
+
+  } else {
+    x <- apply(x, 2, function(h) gsub("-", " ", h))
+  }
+
+  print0("removing punct")
+  x <- apply(x,2,function(h) removePunctuation(h))
+  print0("stripping white space")
+  x <- apply(x,2,function(h) stripWhitespace(h))
+  print0("trimming white space")
+  x <- apply(x,2,function(h) trimws(h))
+  print0("numbers only")
+  x <- apply(x,2,function(h) suppressWarnings(ifelse(numbers_only(h), as.numeric(as.character(h)), h)))
+  print0("fixing years")
+  x <- yrFun(x)
+  print0("done")
+  return(as.data.frame(x))
+}
+
+
+
+#' CSV to Feather (if necessary)
+#'
+#' @param
+#' @keywords
+#' @export
+#' @examples
+
+c2f <- function(in_fn, out_fn=in_fn, clean=F, pipe=F){
+  # f <- read.csv(paste0(in_fn, ".csv"), header=T, stringsAsFactor=F)
+  # assign(f, read.csv(paste0(in_fn, ".csv"), header=T, stringsAsFactor=F), envir=.GlobalEnv)
+  if(pipe){
+    read <- function(...) read.pipe(...)
+  } else {
+    read <- function(...) read.csv(...)
+  }
+
+  if (clean) {
+    print("reading from csv, cleaning, and writing to feather")
+    f <- read(paste0(in_fn, ".csv"), header=T, stringsAsFactor=F)
+    f <- cleanData(f)
+    write_feather0(f, paste0(out_fn, ".feather"))
+  } else {
+    feather_name <- paste0(in_fn,".feather")
+    if(file.exists(feather_name)) {
+      print("feather file already exists, just reading from disk")
+      f <- read_feather0(feather_name)
+    } else {
+      print("file doesn't exist -- reading file from csv, writing to feather")
+      f <- read(paste0(in_fn, ".csv"), header=T, stringsAsFactor=F)
+      write_feather0(f, paste0(out_fn, ".feather"))
+    }
+  }
+  return(f)
 }
