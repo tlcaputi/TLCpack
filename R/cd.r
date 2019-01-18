5fb17f27621b79bc13c13a39d56299c5b3d3564a
Theodore Caputi <tcaputi@gmail.com>
2019-01-18 11:57:38 -0500

    update predict_expand

--- R/cd.r
+++ R/cd.r
@@ -46,32 +46,49 @@ outersect <- function(x, y, ...) {
 #' predict_expand(reg = lm(outcome ~ time), new.time = 1990:2020)
 
 
-predict_expand <- function(reg, new.time){
+predict_expand <- function(reg, new.time, orig.data=NA){
 
-  if(length(all.vars(reg$terms))!=2) stop("Regression must be univariate outcome ~ time")
+  if(length(all.vars(reg$terms))!=2) stop("Regression must be univariate: outcome ~ time")
 
   if(!require(pacman)) install.packages("pacman")
   pacman::p_load(BMisc, devtools, rlang)
 
   outcome_var <- BMisc::lhs.vars(reg$terms)
   time_var <- BMisc::rhs.vars(reg$terms)
-  original_data <- reg$model[,c(time_var, outcome_var)]
 
-  if(all(new.time %in% original_data[,time_var])) {
+  model_data <- reg$model[,c(time_var, outcome_var)]
+
+  if(all(new.time %in% model_data[,time_var])) {
 
     warning("Nothing to impute, returning original data")
-    return(original_data %>% arrange(!!parse_quosure(time_var)))
+    new_data <- orig.data %>% arrange(!!parse_quosure(time_var))
 
   } else {
 
-  names(original_data) <- c("time", "outcome")
-  missing_time <- tlcPack::outersect(new.time, original_data$time)
+  names(model_data) <- c("time", "outcome")
+  missing_time <- tlcPack::outersect(new.time, model_data$time)
   temp_data <- data.frame(time=missing_time, outcome=NA)
-  new_data <- rbind.data.frame(original_data, temp_data)
+  new_data <- rbind.data.frame(model_data, temp_data)
   names(new_data) <- c(time_var, outcome_var)
   new_data$fitted <- predict(reg, newdata=new_data, type="response")
   new_data[,outcome_var] <- ifelse(is.na(new_data[,outcome_var]), new_data$fitted, new_data[,outcome_var])
   new_data <- new_data %>% select(time_var, outcome_var) %>% arrange(!!parse_quosure(time_var))
+
+  }
+
+  if(!is.na(orig.data)){
+
+    if(!all.equal(
+      orig.data[,c(time_var, outcome_var)],
+      reg$model[,c(time_var, outcome_var)])
+    )
+  ) { stop("model data and orig.data not the same") }
+
+    other_variables <- outersect(names(orig.data), all.vars(reg.terms))
+    other_data <- orig.data[,c(other_variables, time_var)]
+    new_data <- merge(new_data, other_data, by=time_var)
+  }
+
   return(new_data)
 
   }
