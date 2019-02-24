2d3ae832484ed18ceab11d0f6058d98625da4f27
Theodore Caputi <tcaputi@gmail.com>
2019-02-24 00:35:24 -0500

    make it okay for c, list, and data.frame to end in commas

---
+++ R/no_commas.r
@@ -0,0 +1,47 @@
+#' Make it acceptable for a function to end in a comma
+#'
+#' @param
+#' @keywords
+#' @export
+#' @examples
+
+ok.comma <- function(FUN) {
+  function(...) {
+    arg.list <- as.list(sys.call())[-1L]
+    len <- length(arg.list)
+    if (len > 1L) {
+      last <- arg.list[[len]]
+      if (missing(last)) {
+        arg.list <- arg.list[-len]
+      }
+    }
+    do.call(FUN, arg.list)
+  }
+}
+
+#' Make it acceptable for the c() function to end in a comma
+#'
+#' @param
+#' @keywords
+#' @export
+#' @examples
+
+c          <- ok.comma(base::c)
+
+#' Make it acceptable for the list() function to end in a comma
+#'
+#' @param
+#' @keywords
+#' @export
+#' @examples
+
+list       <- ok.comma(base::list)
+
+#' Make it acceptable for the data.frame() function to end in a comma
+#'
+#' @param
+#' @keywords
+#' @export
+#' @examples
+
+data.frame <- ok.comma(base::data.frame)
