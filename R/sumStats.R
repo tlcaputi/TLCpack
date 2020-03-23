f002a4c915f21c1ed7d6a5647c41a0c38155ea22
tlcaputi <tcaputi@gmail.com>
2020-03-23 17:21:25 -0400

    made proportions into percentages

--- R/sumStats.R
+++ R/sumStats.R
@@ -7,8 +7,6 @@
 
 ss = function(x,name=""){
 
-  print("NEW SS FUNCTION!")
-
   r = function(x) round(x,3) #this makes it easier to round things
   if(is.numeric(x)){ # for variables that are numeric
     n = sum(!is.na(x)) # I take the N, mean, std dev, sE(mean), and 95%ci
@@ -24,7 +22,7 @@ ss = function(x,name=""){
     for (i in sort(unique(x))){ # I run a for-loop  over each value that collects
       v = ifelse(name!="",paste0(name,"_",i),i) # this creates a varible name to go in the table
       n = sum(I(x==i),na.rm=T) # the in-group N and mean (proportion)
-      mn = mean(I(x==i),na.rm=T)
+      mn = mean(I(x==i),na.rm=T)*100
       ax = prop.test(sum(I(x==i),na.rm=T),sum(!is.na(x))) # I compute a 1-sample prop test
       lo95 = ax$conf.int[1] # so I can extract the confidence interval
       hi95 = ax$conf.int[2]
